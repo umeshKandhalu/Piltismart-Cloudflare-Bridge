3ad2bc9dd13849875066acb39f7cec7e9cd9f0b9
Umesh Kandhalu <umeshkandhalu@Umeshs-MacBook-Pro.local>
2026-08-15 21:42:33 +0100

    chore: remove unneeded thingsboard and fix scripts

--- fix.php
+++
@@ -1,5 +0,0 @@
-<?php
-$f = "/var/www/html/piltistoreapp/kabijewellery_frontend/packages/Webkul/Admin/src/Resources/views/configuration/field-type.blade.php";
-$c = file_get_contents($f);
-$c = str_replace("{{ Storage::url(\$value) }}", "{{ \$value ? Storage::url(\$value) : '' }}", $c);
-file_put_contents($f, $c);
